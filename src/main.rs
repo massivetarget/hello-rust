2676ea6d1cf93d012355761f6d5beabfeb2f00dd
Kamalsinh <kamalsinhd@gmail.com>
2025-01-08 01:14:26 +0530

    Add launch configuration for debugging and enhance main function with user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    std::io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+    let greetingng = guess.contains("Hello");
+    if greetingng {
+        println!("Your guess contains 'Hello'");
+    } else {
+        println!("Your guess does not contain 'Hello'");
+    }
+    
+}
+
+
+/* fn main() {
     println!("Hello, world!");
 }
+ */
+
